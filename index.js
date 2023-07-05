251bbab06389a69f7cfaa1307230c6db5d94168c
unknown <al_jager1983@yahoo.com>
2023-07-05 16:38:50 +0800

    3rd commit: finished mobile, 375px ++ responsive layout

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const scrSize = document.getElementById("wh");
+const burger = document.getElementById("burger");
+const navbar = document.getElementById("navbar");
+const closeNav = document.getElementById("close");
+
+burger.addEventListener("click", function () {
+    burger.style.display = "none";
+    navbar.style.display = "flex";
+})
+
+closeNav.addEventListener("click", function () {
+    burger.style.display = "flex";
+    navbar.style.display = "none";
+})
 
 function size () {
     let h = window.innerHeight;
