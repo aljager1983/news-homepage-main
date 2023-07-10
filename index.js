1c4133c8a6d1df21fa2e5017dd45f96d07d91066
promdiGamer <al_jager1983@yahoo.com>
2023-07-10 10:54:03 +0800

    6-mbile navbar

--- index.js
+++ index.js
@@ -2,19 +2,34 @@ const scrSize = document.getElementById("wh");
 const burger = document.getElementById("burger");
 const navbar = document.getElementById("navbar");
 const closeNav = document.getElementById("close");
+const cover = document.getElementById("cover");
+const close = document.getElementById("close");
+const home = document.getElementById("home");
+const newAside = document.getElementById("new");
+const popular = document.getElementById("popular");
+const trending = document.getElementById("trending");
+const categories = document.getElementById("categories");
 
 burger.addEventListener("click", function () {
     burger.style.display = "none";
     navbar.style.display = "flex";
+    closeNav.style.display = "flex";
+    cover.style.display = "block";
 })
 
 closeNav.addEventListener("click", function () {
-    burger.style.display = "flex";
-    navbar.style.display = "none";
+    closeAll();
 })
+
 
 function size () {
     let h = window.innerHeight;
     let w = window.innerWidth;
     scrSize.innerHTML = w + "x" + h + " wxh";
+}
+
+function closeAll () {
+    burger.style.display = "flex";
+    navbar.style.display = "none";
+    cover.style.display = "none";
 }
